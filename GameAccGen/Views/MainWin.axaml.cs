5a5795094a896fd087a3f08ff965f2b05e28f595
Wastopous <wastopous23@mail.ru>
2024-04-20 07:10:39 +0700

    1

--- GameAccGen/Views/MainWin.axaml.cs
+++ GameAccGen/Views/MainWin.axaml.cs
@@ -12,27 +12,29 @@ namespace GameAccGen.Views;
 public partial class MainWin : Window
 {
     public MySqlConnectionStringBuilder _ConnectionSB;
-   
+
     public MainWin()
     {
         InitializeComponent();
         _ConnectionSB = new MySqlConnectionStringBuilder
         {
-            Server = "localhost", 
-            Database = "gaminggeneration",
-            UserID = "root", 
+            Server = "localhost",
+            Database = "gameaccgen",
+            UserID = "root",
             Password = "1234",
         };
         // if (Login.Text == null || Password.Text == null)
         // {
-        //     LoginButton.IsVisible = false;
+        //     LoginButton.IsEnabled = false;
         // }
         // else
         // {
         //     LoginButton.IsEnabled = true;
         // }
     }
+
     #region RegPanel
+
     private void SignInButton_OnClick(object? sender, RoutedEventArgs e)
     {
         string sql = """
@@ -45,20 +47,22 @@ public partial class MainWin : Window
             using (var com = con.CreateCommand())
             {
                 com.CommandText = sql;
-                com.Parameters.Add("@name", (MySqlDbType.String)); 
-                com.Parameters["@name"].Value = NameTextBox.Text ; 
-                com.Parameters.Add("@lastname", (MySqlDbType.String)); 
-                com.Parameters["@lastname"].Value = SurNameTextBox.Text ; 
-                com.Parameters.Add("@birthdate", (MySqlDbType.Date)); 
-                com.Parameters["@birthdate"].Value = BirthPicker.SelectedDate ; 
-                com.Parameters.Add("@login", (MySqlDbType.String)); 
-                com.Parameters["@login"].Value = LoginTextBox.Text ; 
-                com.Parameters.Add("@pincode", (MySqlDbType.Int32)); 
-                com.Parameters["@pincode"].Value = CodeTextBox.Text ; 
+                com.Parameters.Add("@name", (MySqlDbType.String));
+                com.Parameters["@name"].Value = NameTextBox.Text;
+                com.Parameters.Add("@lastname", (MySqlDbType.String));
+                com.Parameters["@lastname"].Value = SurNameTextBox.Text;
+                com.Parameters.Add("@birthdate", (MySqlDbType.Date));
+                com.Parameters["@birthdate"].Value = BirthPicker.SelectedDate;
+                com.Parameters.Add("@login", (MySqlDbType.String));
+                com.Parameters["@login"].Value = LoginTextBox.Text;
+                com.Parameters.Add("@pincode", (MySqlDbType.Int32));
+                com.Parameters["@pincode"].Value = CodeTextBox.Text;
                 com.ExecuteNonQuery();
             }
+
             con.Close();
-            this.Close();
+            RegPanel.IsVisible = false;
+            LoginPanel.IsVisible = true;
         }
     }
 
@@ -67,14 +71,17 @@ public partial class MainWin : Window
         RegPanel.IsVisible = false;
         LoginPanel.IsVisible = true;
     }
+
     #endregion
+
     #region LoginPanel
+
     private void RegButton_OnClick(object? sender, RoutedEventArgs e)
     {
         LoginPanel.IsVisible = false;
         RegPanel.IsVisible = true;
     }
-    
+
     private void Login_OnKeyDown(object? sender, KeyEventArgs e)
     {
         if ((e.Key >= Key.D0 && e.Key <= Key.D9) || (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) ||
@@ -83,12 +90,18 @@ public partial class MainWin : Window
             e.Handled = true;
         }
     }
+
     private void LoginButton_OnClick(object? sender, RoutedEventArgs e)
     {
-
-
         LoginPanel.IsVisible = false;
         MainPanel.IsVisible = true;
     }
+
     #endregion
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        MainPanel.IsVisible = false;
+        LoginPanel.IsVisible = true;
+    }
 }
